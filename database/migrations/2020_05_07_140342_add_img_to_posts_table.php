643a167cca2a881f5337a5869bf73dd29eee82c7
manuelguido <manuelguido.m@gmail.com>
2020-05-29 01:38:36 -0300

    small frontend changes

--- database/migrations/2020_05_07_140342_add_img_to_posts_table.php
+++ database/migrations/2020_05_07_140342_add_img_to_posts_table.php
@@ -13,7 +13,7 @@ class AddImgToNewsTable extends Migration
      */
     public function up()
     {
-        Schema::table('news', function (Blueprint $table) {
+        Schema::table('posts', function (Blueprint $table) {
             $table->string('img');
         });
     }
@@ -25,7 +25,7 @@ class AddImgToNewsTable extends Migration
      */
     public function down()
     {
-        Schema::table('news', function (Blueprint $table) {
+        Schema::table('posts', function (Blueprint $table) {
             $table->string('img');
         });
     }
